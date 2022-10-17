97711fab6e72909e3d35959bd185657c04846580
Marvvllo <marvtfs@gmail.com>
2022-10-17 12:04:02 +0800

    Added Project Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { PrimaryButton } from "../components/Buttons";
 import Navbar from "../components/Navbar";
 import photograph from "../public/images/photograph.jpg";
+import placeholder from "../public/images/16_9_placeholder.png";
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +20,7 @@ const Home: NextPage = () => {
       {/* Hero Section */}
       <section className="isolate px-4">
         <div className="">
-          <h1 className="text-4xl font-bold text-center -mb-4">
+          <h1 className="heading text-4xl -mb-4 sm:m-0">
             Marvello Nyahu
           </h1>
         </div>
@@ -35,6 +36,42 @@ const Home: NextPage = () => {
             </PrimaryButton>
           </div>
         </div>
+      </section>
+
+      {/* Projects Section */}
+      <section className="flex flex-col items-center my-8 px-4 isolate relative">
+        <h3 className="heading -mb-4">Projects</h3>
+        <Image
+          width={720}
+          height={360}
+          className="-z-10 aspect-video"
+          src={placeholder}
+        />
+        <svg
+          className="my-2 w-8 h-8 "
+          width="14"
+          height="26"
+          viewBox="0 0 14 26"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7 6V11"
+            stroke="#F3F4F6"
+            strokeMiterlimit="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M13 8.35391C13 4.29515 10.3115 1 7.00003 1C3.6885 1 1 4.29515 1 8.35391V17.6461C1 21.7049 3.6885 25 7.00003 25C10.3115 25 13 21.7049 13 17.6461V8.35391V8.35391Z"
+            stroke="#F3F4F6"
+            strokeMiterlimit="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </section>
     </main>
   );
