c8671ebff3b46c6229837bf573161daa56859b58
Marvvllo <marvtfs@gmail.com>
2023-12-14 20:19:01 +0800

    Update Contact Info.

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -71,7 +71,6 @@ const ContactInfo: React.FC = () => {
         font-medium text-black ring-2 ring-white transition-colors
         duration-300 hover:bg-transparent hover:text-white md:w-auto"
           >
-            <p>{email}</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -86,18 +85,18 @@ const ContactInfo: React.FC = () => {
                 d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
               />
             </svg>
+            <p>{email}</p>
           </button>
 
           {/* Instagram Button */}
           <Link
-            href="https://www.instagram.com/marv.11/"
+            href="https://www.instagram.com/nyaiver/"
             className="group mt-2 flex w-full flex-row items-center justify-between 
           gap-2 rounded-md p-2 font-medium text-white
           ring-2 ring-white transition-colors
           duration-300 hover:bg-white hover:text-black md:w-auto"
             target={"_blank"}
           >
-            <p>@marv.11</p>
             <svg
               fill="currentColor"
               width="200px"
@@ -110,6 +109,7 @@ const ContactInfo: React.FC = () => {
               <title>instagram</title>
               <path d="M14.48 11.28c0 0.48-0.4 0.84-0.84 0.84-0.48 0-0.84-0.4-0.84-0.84 0-0.48 0.4-0.84 0.84-0.84 0.44-0.040 0.84 0.36 0.84 0.84zM17.76 11.64v8.72c0 2.48-2.040 4.52-4.52 4.52h-8.72c-2.48 0-4.52-2.040-4.52-4.52v-8.72c0-2.48 2.040-4.52 4.52-4.52h8.72c2.52 0 4.52 2.040 4.52 4.52zM16.080 11.64c0-1.56-1.28-2.84-2.84-2.84h-8.72c-1.56 0-2.84 1.28-2.84 2.84v8.72c0 1.56 1.28 2.84 2.84 2.84h8.72c1.6 0 2.88-1.28 2.88-2.84l-0.040-8.72zM13.52 16c0-2.56-2.080-4.64-4.64-4.64s-4.64 2.080-4.64 4.64 2.080 4.64 4.64 4.64 4.64-2.080 4.64-4.64zM11.84 16c0 1.64-1.32 2.96-2.96 2.96s-2.96-1.32-2.96-2.96 1.32-2.96 2.96-2.96 2.96 1.32 2.96 2.96z" />
             </svg>
+            <p>@nyaiver</p>
           </Link>
 
           {/* Github Button */}
@@ -121,7 +121,6 @@ const ContactInfo: React.FC = () => {
           duration-300 hover:bg-white hover:text-black md:w-auto"
             target={"_blank"}
           >
-            <p>See my GitHub</p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -134,6 +133,7 @@ const ContactInfo: React.FC = () => {
                 d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
               />
             </svg>
+            <p>Marvvllo</p>
           </Link>
         </div>
       </div>
