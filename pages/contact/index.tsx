9e14098d1b324b8f5a384482cbaf6b3aea55a5da
Marvvllo <marvtfs@gmail.com>
2023-12-15 15:40:56 +0700

    Changed IG Username (again).

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -90,7 +90,7 @@ const ContactInfo: React.FC = () => {
 
           {/* Instagram Button */}
           <Link
-            href="https://www.instagram.com/nyaiver/"
+            href="https://www.instagram.com/nyaivu/"
             className="group mt-2 flex w-full flex-row items-center justify-between 
           gap-2 rounded-md p-2 font-medium text-white
           ring-2 ring-white transition-colors
@@ -109,7 +109,7 @@ const ContactInfo: React.FC = () => {
               <title>instagram</title>
               <path d="M14.48 11.28c0 0.48-0.4 0.84-0.84 0.84-0.48 0-0.84-0.4-0.84-0.84 0-0.48 0.4-0.84 0.84-0.84 0.44-0.040 0.84 0.36 0.84 0.84zM17.76 11.64v8.72c0 2.48-2.040 4.52-4.52 4.52h-8.72c-2.48 0-4.52-2.040-4.52-4.52v-8.72c0-2.48 2.040-4.52 4.52-4.52h8.72c2.52 0 4.52 2.040 4.52 4.52zM16.080 11.64c0-1.56-1.28-2.84-2.84-2.84h-8.72c-1.56 0-2.84 1.28-2.84 2.84v8.72c0 1.56 1.28 2.84 2.84 2.84h8.72c1.6 0 2.88-1.28 2.88-2.84l-0.040-8.72zM13.52 16c0-2.56-2.080-4.64-4.64-4.64s-4.64 2.080-4.64 4.64 2.080 4.64 4.64 4.64 4.64-2.080 4.64-4.64zM11.84 16c0 1.64-1.32 2.96-2.96 2.96s-2.96-1.32-2.96-2.96 1.32-2.96 2.96-2.96 2.96 1.32 2.96 2.96z" />
             </svg>
-            <p>@nyaiver</p>
+            <p>@nyaivu</p>
           </Link>
 
           {/* Github Button */}
